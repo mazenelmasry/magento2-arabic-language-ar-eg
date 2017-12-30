4ec5209b832e4f71c82f77999e54820cd55c762f
mazenelmasry <mazenalex@gmail.com>
2017-12-30 02:50:16 +0200

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *                     prodesign_Arabic package
+ *                     NOTICE OF LICENSE
+ *
+ *                     This source file is subject to the MIT License
+ *                     that is bundled with this package in the file LICENSE.txt.
+ *                     It is also available through the world-wide-web at this URL:
+ *                     https://www.prodesign.com/LICENSE.txt
+ *
+ *                     @category  prodesign
+ *                     @package   prodesign_Arabic
+ *                     @copyright Copyright (c) 2017
+ *                     @license   https://www.prodesign.com/LICENSE.txt
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'mageplaza_ar_eg',
+    __DIR__
+);
